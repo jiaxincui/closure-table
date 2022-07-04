8804d83f0d8392dbae92be86bb86e6eb06baefde
xyxu <37847940@qq.com>
2022-07-04 10:09:16 +0800

    add asTree

--- src/Extensions/CollectionExtension.php
+++ src/Extensions/CollectionExtension.php
@@ -22,6 +22,7 @@ class CollectionExtension extends Collection
         foreach ($data as $v) {
             $items[$v[$primary]] = $v;
         }
+
         $tree = array();
         foreach ($items as $item) {
             if (isset($items[$item[$parent]])) {
@@ -32,4 +33,31 @@ class CollectionExtension extends Collection
         }
         return $tree;
     }
+
+    public function asTree($primary = 'id', $parent = 'parent', $children = 'children')
+    {
+        $childArr = [];
+        $childIds = [];
+        $items = $this->keyBy($primary);
+        $items->each(function ($item, $key) use(&$childArr, &$childIds, $items, $primary, $parent) {
+            if (isset($items[$item->$parent])) {
+                $childArr[$item->$parent][$item->$primary] = $items[$item->$primary];
+                array_push($childIds, $item->$primary);
+            }
+        });
+
+        $tree = $items->map(function ($item, $key) use($childArr, $items, $primary, $children) {
+            if (isset($childArr[$item->$primary])) {
+                $item[$children] = $childArr[$item->$primary];
+            };
+            return $item;
+        });
+
+        foreach ($childIds as $id) {
+            unset($tree[$id]);
+        }
+
+        return $tree;
+    }
+
 }
